9cca25965c1ff4bcec15697c8831470ccaa2adba
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-03-02 15:12:06 +0100

    Update .gitignore

---
+++ out/artifacts/build/release.ps1
@@ -0,0 +1,69 @@
+$destinationZip = "..\releases\latest.zip"
+$work_dir = "Elfin Connect Desktop"
+$java_folder = Join-Path $work_dir "java"
+$download_link = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.zip"
+
+if (-not (Test-Path -Path $work_dir)) {
+    New-Item -ItemType Directory -Path $work_dir | Out-Null
+    Write-Host "Created components folder."
+}
+
+
+# Check if the java folder exists
+if (-not (Test-Path $java_folder)) {
+    Write-Host "Java folder not found. Downloading Java..."
+
+    # Create the desktop folder if it doesn't exist
+    New-Item -ItemType Directory -Path $work_dir -ErrorAction SilentlyContinue
+
+    # Download Java zip file
+    Invoke-WebRequest -Uri $download_link -OutFile (Join-Path -Path $work_dir -ChildPath java.zip)
+
+    # Unzip the Java file
+    Expand-Archive -Path (Join-Path -Path $work_dir -ChildPath java.zip) -DestinationPath $work_dir
+
+    # Rename the extracted folder to "java"
+    Rename-Item -Path (Join-Path $work_dir "jdk-21.0.2+13-jre") -NewName "java"
+
+    # Clean up - remove the downloaded zip file
+    Remove-Item -Path (Join-Path -Path $work_dir -ChildPath java.zip)
+
+    Write-Host "Java hooked successfully."
+} else {
+    Write-Host "Java folder already exists. No action needed."
+}
+
+# Check if files were copied
+$artifactsFiles = Get-ChildItem -Path ../elfinConnectDesktop_jar
+Copy-Item -Path ../elfinConnectDesktop_jar/* -Destination $work_dir
+
+if ($artifactsFiles.Count -gt 0){
+	Write-Host "Artifacts files copied."
+} else {
+    Write-Host "Artifacts files are missing."
+}
+
+# Create gfx folder if it doesn't exist
+$gfxPath = Join-Path -Path $work_dir -ChildPath gfx
+if (-not (Test-Path -Path $gfxPath)) {
+    New-Item -ItemType Directory -Path $gfxPath | Out-Null
+    Write-Host "Created gfx folder."
+}
+Copy-Item -Path ../../../gfx/* -Destination $gfxPath -Recurse
+Write-Host "GFX files copied."
+
+Copy-Item -Path "Elfin Connect Desktop.exe" -Destination $work_dir
+Write-Host "Launcher file copied."
+
+# Zip the folder
+Compress-Archive -Path $work_dir -DestinationPath $destinationZip -Force
+
+# Check if the archive was created successfully
+if (Test-Path $destinationZip) {
+    Write-Host "Folder has been zipped successfully."
+} else {
+    Write-Host "Failed to zip the folder."
+}
+
+Remove-Item -Path $work_dir -Force -Recurse
+
---
+++ out/artifacts/build/release.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+destinationZip="../releases/latest.zip"
+work_dir="Elfin Connect Desktop"
+java_folder="$work_dir/java"
+download_link="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.2%2B13/OpenJDK21U-jre_x64_windows_hotspot_21.0.2_13.zip"
+
+# Check if the work directory exists
+if [ ! -d "$work_dir" ]; then
+    mkdir -p "$work_dir"
+    echo "Created components folder."
+fi
+
+# Check if the Java folder exists
+if [ ! -d "$java_folder" ]; then
+    echo "Java folder not found. Downloading Java..."
+
+    # Create the desktop folder if it doesn't exist
+    mkdir -p "$work_dir"
+
+    # Download Java zip file
+    wget -O "$work_dir/java.zip" "$download_link"
+
+    # Unzip the Java file
+    unzip -q "$work_dir/java.zip" -d "$work_dir"
+
+    # Rename the extracted folder to "java"
+    mv "$work_dir/jdk-21.0.2+13-jre" "$java_folder"
+
+    # Clean up - remove the downloaded zip file
+    rm "$work_dir/java.zip"
+
+    echo "Java hooked successfully."
+else
+    echo "Java folder already exists. No action needed."
+fi
+
+# Check if files were copied
+artifactsFiles=(../elfinConnectDesktop_jar/*)
+cp -r ../elfinConnectDesktop_jar/* "$work_dir"
+
+if [ ${#artifactsFiles[@]} -gt 0 ]; then
+    echo "Artifacts files copied."
+else
+    echo "Artifacts files are missing."
+fi
+
+# Create gfx folder if it doesn't exist
+gfxPath="$work_dir/gfx"
+if [ ! -d "$gfxPath" ]; then
+    mkdir -p "$gfxPath"
+    echo "Created gfx folder."
+fi
+cp -r ../../../gfx/* "$gfxPath"
+echo "GFX files copied."
+
+cp "Elfin Connect Desktop.exe" "$work_dir"
+echo "Launcher file copied."
+
+# Zip the folder
+zip -r "$destinationZip" "$work_dir"
+
+# Check if the archive was created successfully
+if [ -f "$destinationZip" ]; then
+    echo "Folder has been zipped successfully."
+else
+    echo "Failed to zip the folder."
+fi
